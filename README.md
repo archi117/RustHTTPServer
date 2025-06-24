ae1871168873ba668a86c0fbac6da6758d049eef
archi <pedrojv117@gmail.com>
2025-06-24 08:43:02 -0600

    Obtenemos el  servidor básico con hyper

---
+++ README.md
@@ -0,0 +1,2 @@
+# Http Server Rust
+Proyecto en el cual hacemos un servidor http multihilo usando la librería de [Hyper](https://hyper-rs.translate.goog/?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=tc), Esto con el fín de tener un sistema distribuido que maneje distintos servicios.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,84 @@
-pub mod tcp_server;
+// Librerías necesarias
+use std::net::SocketAddr;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+use hyper::{Method, StatusCode};
 
-use std::{io, net::SocketAddr};
-use crate::tcp_server::ServidorTcp;
+//use hyper::body::Frame;
 
 
-fn main() -> Result<(), io::Error> {
-    let server = match ServidorTcp::new(SocketAddr::from(([127, 0, 0, 1], 80))) {
-        Ok(s) => s,
-        Err(e) => panic!("Error en la conección al servidor \n{}", e),
-    };
+// Funcion de ejemplo
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    Ok(Response::new(BoxBody::new(full("Hello, World!"))))
 
-    println!("Servidor conectado a {}", server.addr);
+}
 
-    for stream in server.listener.incoming() {
-        ServidorTcp::handle(stream?)?;
+// Tabla para hacer el ruteo
+async fn routing(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full(
+            "Try POSTing data to /echo",
+        ))),
+
+        //(&Method::POST, "/echo") => {
+            // we'll be back
+        //},
+
+        (&Method::GET, "/hello") => hello(req).await,
+
+
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
     }
+}
 
-    Ok(())
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+  
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(routing))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
